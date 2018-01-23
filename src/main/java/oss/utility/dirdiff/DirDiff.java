c7a9323e30cff06300a9e63eebf979940b4efc62
sosmolovskii <sosmolovskii@gmail.com>
2018-01-24 02:57:12 +0300

    Add checking empty folders - green dummy test

--- src/main/java/oss/utility/dirdiff/DirDiff.java
+++ src/main/java/oss/utility/dirdiff/DirDiff.java
@@ -1,11 +1,20 @@
 package oss.utility.dirdiff;
 
 import java.io.File;
+import java.util.Set;
 
 public class DirDiff {
 
     public static void compare(File init, File toCompare, File diff) {
 
+        Set<File> initFiles = (init.listFiles() != null) ? Set.of(init.listFiles()) : Set.of();
+        Set<File> filesToCompare = (toCompare.listFiles() != null) ? Set.of(toCompare.listFiles()) : Set.of();
+
+        if (initFiles.isEmpty()) {
+            if (filesToCompare.isEmpty()) {
+                return;
+            }
+        }
 
     }
 
